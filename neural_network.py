ce146ee75eed0851715ceaceb6c6de62a3fbca50
Mattia Riboni <mattiariboni@gmail.com>
2025-07-25 15:08:02 +0200

    first commito

---
+++ neural_network.py
@@ -0,0 +1,141 @@
+from torch.utils.data import Dataset, DataLoader
+import torch.nn as nn
+import torch
+from sklearn.metrics import f1_score, accuracy_score
+
+class TabularDataset(Dataset):
+    def __init__(self, X_num, X_cat, y):
+        self.X_num = X_num
+        self.X_cat = X_cat
+        self.y = y
+
+    def __len__(self):
+        return len(self.y)
+
+    def __getitem__(self, idx):
+        return self.X_num[idx], self.X_cat[idx], self.y[idx] 
+    
+
+class NeuralNetwork(nn.Module):
+
+
+    
+    def __init__(self, hidden_layers_sizes, cat_cardinalities, embedding_dims, num_numerical_features, num_target_classes):
+        super().__init__()
+
+
+        self.embeddings = nn.ModuleList([
+            nn.Embedding(cat_cardinality, embedding_dim)
+            for cat_cardinality, embedding_dim in zip(cat_cardinalities, embedding_dims)
+        ])
+
+
+        total_embedding_size = sum(embedding_dims)
+        input_size = total_embedding_size + num_numerical_features               #  Number of neurons in the input layer
+        layers_dims = [input_size] + hidden_layers_sizes + [num_target_classes]  #  Number of neurons in each layer
+        layers = []
+
+
+        for i in range(len(layers_dims) - 1):
+            layers.append(nn.Linear(layers_dims[i], layers_dims[i + 1]))
+
+            # Output layer without ReLU, BatchNorm and Dropout #
+            if i < len(layers_dims) - 2:      
+                layers.append(nn.BatchNorm1d(layers_dims[i+1]))                 
+                layers.append(nn.Dropout(0.2))
+                layers.append(nn.ReLU())
+
+        self.network = nn.Sequential(*layers)
+
+
+
+
+    def forward(self, x_num, x_cat):
+        embedded_cat = []
+
+        for i, emb in enumerate(self.embeddings):
+            column_embedded = emb(x_cat[:,i])                                    #  Gets i-th column in batch and applies embedding
+            embedded_cat.append(column_embedded)
+
+
+        x_cat = torch.cat(embedded_cat, dim=1)
+        x = torch.cat([x_num,x_cat], dim=1)
+
+        return self.network(x)
+
+
+    def fit(self, train_dataloader, valid_dataloader, device, epochs=20, loss_fn=nn.CrossEntropyLoss(), lr=1e-3 ,optimizer=None):
+        
+        if optimizer is None:
+            optimizer = torch.optim.Adam(self.network.parameters(), lr=lr)
+
+        for epoch in range(epochs):
+            self.network.train()
+
+            for x_num, x_cat, y in train_dataloader:
+                x_num, x_cat, y = x_num.to(device), x_cat.to(device), y.to(device)
+                
+                preds = self(x_num, x_cat)
+                loss = loss_fn(preds, y)
+
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+
+            score, acc = self.evaluate(valid_dataloader, device)
+            print(f"--- Epoch: {epoch}  |  F1 Score: {score:.4f}  |  Accuracy: {acc:.4f} ---")
+
+
+    def evaluate(self, dataloader, device):
+
+        self.eval()
+        
+        all_preds = []
+        all_labels = []
+
+        with torch.no_grad():
+            for x_num, x_cat, y in dataloader:
+                x_num, x_cat, y = x_num.to(device), x_cat.to(device), y.to(device)
+                outputs = self(x_num, x_cat)
+                preds = outputs.argmax(dim=1)
+
+                all_preds.append(preds.cpu())
+                all_labels.append(y.cpu())
+
+        all_preds = torch.cat(all_preds).numpy()
+        all_labels = torch.cat(all_labels).numpy()
+
+        return f1_score(all_labels, all_preds, average='macro'), accuracy_score(all_preds,all_labels)
+    
+
+    
+    def predict(self, dataloader, device):
+        self.eval()
+        all_preds = []
+
+        with torch.no_grad():
+            for x_num, x_cat, _ in dataloader:  
+                x_num, x_cat = x_num.to(device), x_cat.to(device)
+                outputs = self(x_num, x_cat)
+                preds = outputs.argmax(dim=1)
+                all_preds.append(preds.cpu())
+
+        return torch.cat(all_preds)
+
+
+
+    def save(self, path, config=None):
+        torch.save({
+            'state_dict': self.state_dict(),
+            'config': config 
+        }, path)
+
+
+    @classmethod
+    def load(cls, path):
+        checkpoint = torch.load(path)
+        model = cls(**checkpoint['config'])
+        model.load_state_dict(checkpoint['state_dict'])
+        model.eval()
+        return model
+
---
+++ utils.py
@@ -0,0 +1,141 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import torch
+from neural_network import TabularDataset
+from torch.utils.data import DataLoader
+
+def load_data(file_path):
+    """
+    Load data from a CSV file into a pandas DataFrame.
+    
+    Parameters:
+    file_path (str): The path to the CSV file.
+    
+    Returns:
+    pd.DataFrame: A DataFrame containing the loaded data.
+    """
+    try:
+        data = pd.read_csv(file_path)
+        return data
+    except Exception as e:
+        print(f"Error loading data: {e}")
+        return None
+    
+
+def split_data(data, target_col, random_state=42):
+    """
+    Split the data into training and testing sets.
+    
+    Parameters:
+    data (pd.DataFrame): The DataFrame to split.
+    target_col: Name of target column.
+    
+    Returns:
+    tuple: A tuple containing the training, valid and testing DataFrames.
+    """
+    train_df, temp_df = train_test_split(data, test_size=0.3, random_state=random_state, stratify=data[target_col])
+    valid_df, test_df = train_test_split(temp_df, test_size=0.5, random_state=random_state, stratify=temp_df[target_col])   
+    
+    return train_df, valid_df, test_df
+
+def remove_rows(data, values_to_remove):
+    
+    for col, values in values_to_remove.items():
+        for val in values:
+            data = data[data[col] != val]
+
+    return data
+
+def keep_columns(data, columns_to_keep):
+    """
+    Remove unnecessary columns from the DataFrame.
+    
+    Parameters:
+    data (pd.DataFrame): The DataFrame from which to remove columns.
+    columns_to_keep (list): List of column names to keep.
+    
+    Returns:
+    pd.DataFrame: The DataFrame with specified columns removed.
+    """
+    return data[columns_to_keep]
+
+def get_X_num(data, numerical_cols):
+    """
+    Get a DataFrame containing only the specified numeric columns.
+    
+    Parameters:
+    data (pd.DataFrame): The original DataFrame.
+    numerical_cols (list): List of numeric column names to include.
+    
+    Returns:
+    pd.DataFrame: A DataFrame with only the specified numeric columns.
+    """
+    return data[numerical_cols]
+
+def get_X_cat(data, categorical_cols):
+    """
+    Get a DataFrame containing only the specified categorical columns.
+    
+    Parameters:
+    data (pd.DataFrame): The original DataFrame.
+    categorical_cols (list): List of categorical column names to include.
+    
+    Returns:
+    pd.DataFrame: A DataFrame with only the specified categorical columns.
+    """
+    return data[categorical_cols]
+
+
+def load_and_prepare_data(file_path, target_col, numerical_cols, categorical_cols, rows_to_remove):
+    """
+    Load and prepare the data for modeling.
+    
+    Parameters:
+    file_path (str): The path to the CSV file.
+    target_col: Name of target column.
+    numerical_cols (list): List of numeric column names.
+    categorical_cols (list): List of categorical column names.
+    columns_to_keep (list): List of columns to keep in the final DataFrame.
+    
+    Returns:
+    Dataloader: Train, valid and test dataloaders
+    """
+    data = load_data(file_path)
+    if data is None:
+        return None, None, None
+    
+    data = keep_columns(data, numerical_cols + categorical_cols + [target_col])
+    data = remove_rows(data, rows_to_remove)
+
+    for col in categorical_cols + [target_col]:
+        data[col] = data[col].astype('category').cat.codes
+
+    cat_cardinalities = [data[col].nunique() for col in categorical_cols]
+
+    train_df, valid_df, test_df = split_data(data, target_col)
+    
+    X_train_num = torch.tensor(get_X_num(train_df, numerical_cols).values, dtype=torch.float32)
+    X_valid_num = torch.tensor(get_X_num(valid_df, numerical_cols).values, dtype=torch.float32)
+    X_test_num = torch.tensor(get_X_num(test_df, numerical_cols).values, dtype=torch.float32)
+    
+    X_train_cat = torch.tensor(get_X_cat(train_df, categorical_cols).values, dtype=torch.long)
+    X_valid_cat = torch.tensor(get_X_cat(valid_df, categorical_cols).values, dtype=torch.long)
+    X_test_cat = torch.tensor(get_X_cat(test_df, categorical_cols).values, dtype=torch.long)
+
+    y_train = torch.tensor(train_df[target_col].values, dtype=torch.long)
+    y_valid = torch.tensor(valid_df[target_col].values, dtype=torch.long)
+    y_test = torch.tensor(test_df[target_col].values, dtype=torch.long)
+
+    train_dataset = TabularDataset(X_train_num, X_train_cat, y_train)
+    valid_dataset = TabularDataset(X_valid_num, X_valid_cat, y_valid)
+    test_dataset = TabularDataset(X_test_num, X_test_cat, y_test)
+
+    train_dataloader = DataLoader(train_dataset, batch_size=256, shuffle=True)
+    valid_dataloader = DataLoader(valid_dataset, batch_size=256)
+    test_dataloader = DataLoader(test_dataset, batch_size=256)
+
+
+    
+    return train_dataloader, valid_dataloader, test_dataloader, cat_cardinalities
+
+
